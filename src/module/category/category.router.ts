bb782b1b559408cf7708924a9356e3f7ffeea44a
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-11-17 05:38:05 +0600

    verify jwt added

--- src/module/category/category.router.ts
+++ src/module/category/category.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyCategory } from "./category.validation";
 import { createCategory, getCategory } from "./category.controller";
+import { verifyJwt } from "../../middleware/verifyJwt";
 
 const router = Router();
 
 router.post('/create-category', verifyCategory, createCategory)
-router.get('/get-category',  getCategory)
+router.get('/get-category', verifyJwt,  getCategory)
 
 export default router;
